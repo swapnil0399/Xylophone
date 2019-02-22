9d9d4b4cae26bf1bc5758c50a80faf9588527d9f
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-02-22 02:54:10 -0600

    Add files

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer : AVAudioPlayer?
+    var soundNodes = ["note1","note2","note3","note4","note5","note6","note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        guard let url = Bundle.main.url(forResource: soundNodes[sender.tag], withExtension: "wav") else {return}
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let audioPlayer = audioPlayer else { return }
+            
+            audioPlayer.play()
+            
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+  
+
+}
+
